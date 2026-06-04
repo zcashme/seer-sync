689bdf0142d5d76c4793cfc6ed0664fe17d43d9d
Julian Abraham <julian.abraham@xaviers.edu.in>
2026-06-04 18:09:11 +0800

    fix(db): update sync to use Note/ShieldedNote, drop Tx/Transactions

--- src/db/sync.rs
+++ src/db/sync.rs
@@ -3,7 +3,7 @@ use zcash_protocol::consensus::Network;
 
 use crate::db::{Db, OrchardNoteInsert, SaplingNoteInsert, SyncState};
 use crate::sync::chain::LwdClient;
-use crate::sync::scan::{Transactions, Tx};
+use crate::sync::scan::{Note, ShieldedNote};
 use crate::{BlockHeight, UnifiedFullViewingKey};
 
 pub async fn sync_to_tip(
@@ -37,8 +37,8 @@ pub async fn sync_to_tip(
             }
         },
         |to| db.rewind_to_height(u32::from(to)).context("rewinding after reorg"),
-        |height, hash, txs| {
-            apply(db, height, hash, txs)?;
+        |height, hash, notes| {
+            apply(db, height, hash, notes)?;
             progress(height);
             Ok(())
         },
@@ -48,57 +48,39 @@ pub async fn sync_to_tip(
     Ok(db.get_sync_state().context("reading final cursor")?.height)
 }
 
-fn apply(db: &Db, height: BlockHeight, hash: [u8; 32], txs: &Transactions) -> Result<()> {
-    for tx in &txs.orchard {
-        match tx {
-            Tx::Receive(r) => {
-                let id = db.upsert_transaction(&r.txid, Some(u32::from(r.height)), Some(r.tx_index))?;
+fn apply(db: &Db, height: BlockHeight, hash: [u8; 32], notes: &[Note]) -> Result<()> {
+    for note in notes {
+        let id = db.upsert_transaction(&note.txid, Some(u32::from(note.height)), Some(note.tx_index))?;
+        match &note.note {
+            ShieldedNote::Orchard(n) => {
                 db.insert_orchard_note(&OrchardNoteInsert {
                     transaction_id: id,
-                    action_index: r.output_index,
-                    diversifier: r.recipient.diversifier().as_array(),
-                    value: r.note.value().inner(),
-                    rho: &r.note.rho().to_bytes(),
-                    rseed: r.note.rseed().as_bytes(),
-                    nf: r.nf.as_ref().map(|n| n.as_slice()),
-                    is_change: r.is_change,
-                    memo: r.memo.as_deref().map(|m| m.as_slice()),
-                    commitment_tree_position: r.position,
+                    action_index: note.output_index,
+                    diversifier: n.recipient().diversifier().as_array(),
+                    value: n.value().inner(),
+                    rho: &n.rho().to_bytes(),
+                    rseed: n.rseed().as_bytes(),
+                    nf: Some(&note.nullifier),
+                    is_change: note.is_change,
+                    memo: note.memo.as_ref().map(|m| m.as_slice()),
+                    commitment_tree_position: None,
                 })?;
             }
-            Tx::Spend(s) => {
-                if db.owns_orchard_nf(&s.nf)? {
-                    let id = db.upsert_transaction(&s.txid, Some(u32::from(s.height)), Some(s.tx_index))?;
-                    db.mark_orchard_spent(&s.nf, id)?;
-                }
-            }
-        }
-    }
-    for tx in &txs.sapling {
-        match tx {
-            Tx::Receive(r) => {
-                let id = db.upsert_transaction(&r.txid, Some(u32::from(r.height)), Some(r.tx_index))?;
+            ShieldedNote::Sapling(n) => {
                 db.insert_sapling_note(&SaplingNoteInsert {
                     transaction_id: id,
-                    output_index: r.output_index,
-                    diversifier: &r.recipient.diversifier().0,
-                    value: r.note.value().inner(),
-                    rcm: &r.note.rcm().to_bytes(),
-                    nf: r.nf.as_ref().map(|n| n.as_slice()),
-                    is_change: r.is_change,
-                    memo: r.memo.as_deref().map(|m| m.as_slice()),
-                    commitment_tree_position: r.position,
+                    output_index: note.output_index,
+                    diversifier: &n.recipient().diversifier().0,
+                    value: n.value().inner(),
+                    rcm: &n.rcm().to_bytes(),
+                    nf: Some(&note.nullifier),
+                    is_change: note.is_change,
+                    memo: note.memo.as_ref().map(|m| m.as_slice()),
+                    commitment_tree_position: None,
                 })?;
-            }
-            Tx::Spend(s) => {
-                if db.owns_sapling_nf(&s.nf)? {
-                    let id = db.upsert_transaction(&s.txid, Some(u32::from(s.height)), Some(s.tx_index))?;
-                    db.mark_sapling_spent(&s.nf, id)?;
-                }
             }
         }
     }
-
     db.set_sync_state(&SyncState { height: u32::from(height), hash: Some(hash) })?;
     Ok(())
 }
